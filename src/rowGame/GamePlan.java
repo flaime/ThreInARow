ba132c25f1c4dd7dede9add31de902f0be749998
flaime <ah.linus@gmail.com>
2014-06-09 13:10:34 +0200

    Add the game plan

---
+++ src/rowGame/GamePlan.java
@@ -0,0 +1,26 @@
+package rowGame;
+
+public class GamePlan {
+	//lista för att komma ihåg senaste dragen
+	//ArrayList<EttDrag> dragen = new ArrayList<>();
+	//Spelare [][] spelplan;
+
+	/*
+	 * [0,0][0,1][0,2]
+	 * [1,0][1,1][1,2]
+	 * [2,0][2,1][2,2]
+	 * 
+	 */ //den är ju vänd fe!
+
+	private int height = 0;
+	private int width = 0;
+	
+	public GamePlan(int height, int width) {
+		this.width = width;
+		this.height = height;
+	}
+
+	
+	
+
+}
