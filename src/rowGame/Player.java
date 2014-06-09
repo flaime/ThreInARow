b038720bea5b0271ce3fc1afce64d691aa9fb284
flaime <ah.linus@gmail.com>
2014-06-09 13:30:15 +0200

    Added code to generate tecken to each player

--- src/rowGame/Player.java
+++ src/rowGame/Player.java
@@ -1,13 +1,25 @@
 package rowGame;
 
 public class Player {
-	
+
 	GamePlan gp;
 	String name = "";
-	
+
+	static int numberOfPlayers = 0;
+	String teken = "No tecken has ben given";
+
 	public Player(String name, GamePlan referens) {
 		this.name = name;
 		this.gp = referens;
+
+		// to sett the "tecken"
+		if (numberOfPlayers == 0) {
+			teken = "X";
+		} else if (numberOfPlayers == 1)
+			teken = "O";
+		else
+			teken = "P" + numberOfPlayers;
+		numberOfPlayers++;
 	}
 
 }
