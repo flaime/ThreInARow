557b8b03e97e1e262ab16d679a1e4dd89c51b2aa
flaime <ah.linus@gmail.com>
2014-06-09 12:10:42 +0200

    Make the first struktur

---
+++ src/rowGame/MainClass.java
@@ -0,0 +1,28 @@
+package rowGame;
+
+public class MainClass {
+
+	public MainClass() {
+		
+		StartGame();
+		
+		StartLope();
+		
+		EndGame();
+	}
+
+	private void EndGame() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void StartLope() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void StartGame() {
+		// TODO Auto-generated method stub
+		
+	}
+}
