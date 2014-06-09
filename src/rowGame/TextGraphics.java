5bf95f617e09d05242f6661b1499250699811aaa
flaime <ah.linus@gmail.com>
2014-06-10 01:25:23 +0200

    UppdateeGapichs

--- src/rowGame/TextGraphics.java
+++ src/rowGame/TextGraphics.java
@@ -2,14 +2,47 @@ package rowGame;
 
 public class TextGraphics extends Graphics {
 
+	boolean helpCoordinates = true;
+	
 	public TextGraphics(GamePlan gp) {
 		super(gp);
 		
 	}
 
+	public void sethelpCoordinates(boolean b){
+		helpCoordinates = b;
+	}
+	
 	@Override
 	public void updateGraphics() {
-		
+		//if (sp.equals(spTillfälig) == false) {
+		String print = "";
+
+		for (int i = 0; i < gp.getHeight(); i++) {
+			for (int j = 0; j < gp.getWhite(); j++) {
+				
+				print = print + "[";
+				
+				if (gp.getPosition(i, j) != null)
+					print = print + sp.getPlatts(j, i).getTecken();//.toString();//x:y
+				else{
+					if(helpCoordinates == false)
+						print = print + " ";
+					else
+						print = print + (++j) + ":"+ (++i);
+					--i;
+					--j;
+				}
+				//System.out.println(spTillfälig.getPlatts(i, j));
+				print = print + "]";
+			}
+			print = print + "\n";
+		}
+		System.out.println(print);
+	//}
+	
+
+	//spTillfälig = sp;
 		
 	}
 
