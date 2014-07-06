782963d5b1b4608f5a419f3c1f5f43e11146e738
flaime <ah.linus@gmail.com>
2014-07-06 20:30:48 +0200

    looking after profit horizontally and vertically

--- src/rowGame/MainClass.java
+++ src/rowGame/MainClass.java
@@ -4,74 +4,115 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MainClass {
-	
+
 	ArrayList<Player> player = new ArrayList<>();
-	
+
 	GamePlan gp;
-	
+
 	Graphics gr;
 
 	public MainClass() {
-		
+
 		StartGame();
 		StartLope();
 		EndGame();
 	}
 
-
 	private void StartGame() {
 		gp = new GamePlan(3, 3);
 		player.add(new TextPlayer("palyer 1", gp));
-		player.add(new TextPlayer("palyer 2", gp));		
+		player.add(new TextPlayer("palyer 2", gp));
 		gr = new TextGraphics(gp);
 	}
-	
+
 	private void StartLope() {
-		
+
 		Scanner s = new Scanner(System.in);
-		
-		while(true){
-			//a lop to get the game going
-			
-			if(gr != null){
+
+		while (true) {
+			// a lop to get the game going
+
+			if (gr != null) {
 				gr.updateGraphics();
 			}
-			
-			//add controll to not be abel to lay on a alredy layd plase
-			
+
+			// add controll to not be abel to lay on a alredy layd plase
+
 			gp.setPosition(PlayPlayer());
-				
-			//Do somting to be shor it vill go throt ewrey playewr
-			//and do somting if the players mov isent is ok
-			
-			//Uppdatete grapichs
-			
-			
-			//Lock for vinst
-			if(lookForProfit() != null){
-				//do something
-				gr.profit(lookForProfit());
+
+			// Do somting to be shor it vill go throt ewrey playewr
+			// and do somting if the players mov isent is ok
+
+			// Uppdatete grapichs
+
+			// Lock for vinst
+			Player vinner = lookForProfit();
+			if (vinner != null) {
+				gr.updateGraphics();
+				gr.profit(vinner);
 				break;
 			}
 		}
-		
+
 	}
-	
-	int playerTurn  = -1;
-	private OneMove PlayPlayer(){
+
+	int playerTurn = -1;
+
+	private OneMove PlayPlayer() {
 		playerTurn++;
-		if((playerTurn) == player.size())
+		if ((playerTurn) == player.size())
 			playerTurn = 0;
 		return player.get(playerTurn).makeMove();
 	}
-	
-	private Player lookForProfit(){
-		return player.get(0);
-		//do somthing her
+
+	private Player lookForProfit() {
+		Player p = null;
+
+		// vågräta vinster
+		for (int y = 0; y < gp.getHeight(); y++) {
+
+			ArrayList<Player> tempPlayerX = new ArrayList<>();
+			for (int x = 0; x < gp.getWhite(); x++) {
+				tempPlayerX.add(gp.getPosition(x, y));
+			}
+			if (isTheSame(tempPlayerX) == true)
+				p = tempPlayerX.get(0);
+
+		}
+		// lodräta vinster
+
+		for (int x = 0; x < gp.getWhite(); x++) {
+
+			ArrayList<Player> tempPlayerY = new ArrayList<>();
+			for (int y = 0; y < gp.getHeight(); y++) {
+				tempPlayerY.add(gp.getPosition(x, y));
+			}
+			if (isTheSame(tempPlayerY) == true)
+				p = tempPlayerY.get(0);
+
+		}
+		// tvära vinster
+
+		return p;
+		// do somthing her
 	}
-	
+
+	private boolean isTheSame(ArrayList<Player> list) {
+		for (int i = 0; i < list.size(); i++) {
+			for (int j = 0; j < list.size(); j++) {
+				if (list.get(j) != list.get(i)) {
+					return false;
+				} else if (list.get(i) == null) {
+					return false;
+				}
+			}
+
+		}
+		return true;
+
+	}
+
 	private void EndGame() {
-		
-		
+
 	}
 }
