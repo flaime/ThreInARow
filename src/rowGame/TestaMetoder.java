5c039b20561bb7d5c0e73f6b5de3a40a6e54cf21
flaime speldatorn <ah.linus@gmail.com>
2014-08-02 15:09:18 +0200

    corrected indexation

--- src/rowGame/TestaMetoder.java
+++ src/rowGame/TestaMetoder.java
@@ -11,48 +11,50 @@ public class TestaMetoder {
 		for (int i = 0; i < 10; i++) {
 			spelare.add(new TextPlayer("player " + i, gp));
 		}
-		
-		spelare.add(spelare.get(1));
-		spelare.add(spelare.get(1));
-		spelare.add(spelare.get(1));
+
 //		spelare.add(spelare.get(1));
-//		spelare.add(spelare.get(3));
-		
+		spelare.add(spelare.get(1));
+		spelare.add(spelare.get(1));
+//		 spelare.add(spelare.get(1));
+		 spelare.add(spelare.get(3));
+
 		for (int i = 0; i < spelare.size(); i++) {
 			System.out.println(spelare.get(i).name);
 		}
-		
-//		TextPlayer tp = new TextPlayer("player " + 10, gp);
-//		spelare.add(tp);
-		
+
+		// TextPlayer tp = new TextPlayer("player " + 10, gp);
+		// spelare.add(tp);
+
 		System.out.println("\n" + isTheSame(spelare, 3));
 	}
 
 	private static boolean isTheSame(ArrayList<Player> list, int xInARow) {
 
 		boolean numberTheSame = false;
-		for (int k = 0; k < list.size() - xInARow; k++) {
+
+		for (int k = 0; k < list.size() - xInARow + 1; k++) {
 
 			ArrayList<Player> tempList = new ArrayList<>();
 
 			System.out.println("----");
 			for (int i = 0; i < xInARow; i++) {
 				tempList.add(list.get(i + k));
-				
+
 				System.out.println(list.get(i + k));
 			}
 
-			//läg till något för att göra längden på listan rätt om det ska vara 3 eller 4 osv
-			if(isTheSame(tempList) == true){
+			// läg till något för att göra längden på listan rätt om det ska
+			// vara 3 eller 4 osv
+			if (isTheSame(tempList) == true) {
 				numberTheSame = true;
 				break;
 			}
-			
+
 		}
 		return numberTheSame;
 
 	}
-	
+
 	private static boolean isTheSame(ArrayList<Player> list) {
 		for (int i = 0; i < list.size(); i++) {
 			for (int j = 0; j < list.size(); j++) {
