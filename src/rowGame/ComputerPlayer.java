ece87e67dd9f8e965d7dc547e44b0ece2add6d34
flaime speldatorn <ah.linus@gmail.com>
2014-08-09 17:14:20 +0200

    Computer player added

---
+++ src/rowGame/ComputerPlayer.java
@@ -0,0 +1,16 @@
+package rowGame;
+
+public class ComputerPlayer extends Player{
+
+	public ComputerPlayer(String name, GamePlan referens) {
+		super(name, referens);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public OneMove makeMove() {
+		//start working her
+		return null;
+	}
+
+}
